521117107add63e67dfa67b3f7e325e71da3a403
Sheila01 <nansheila12@gmail.com>
2019-09-02 17:57:20 +0300

    Added home page

--- green/routes.py
+++ green/routes.py
@@ -1,7 +1,8 @@
 import os 
-from flask import render_template, url_for
+from flask import render_template, url_for,flash, redirect,abort
 from green import app # importing the app from the init.py file (connecting the two)
 from green.forms import RegistrationForm, LoginForm
+from green.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -24,15 +25,17 @@ def register():
 
 @app.route('/login', methods=["GET","POST"])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
+	# if current_user.is_authenticated:
+	# 	return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():		
-		user = User.query.filter_by(email=form.email.data).first()
-		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember=form.remember.data)
-			next_page = request.args.get('next')
-			return redirect(next_page) if next_page else redirect(url_for('home'))
+		if form.email.data == 'email@email.com' and form.password.data == 'password':
+			flash(f'Welcome {form.email.data}!')
+		# user = User.query.filter_by(email=form.email.data).first()
+		# if user and bcrypt.check_password_hash(user.password, form.password.data):
+		# 	login_user(user, remember=form.remember.data)
+		# 	next_page = request.args.get('next')
+		# 	return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful. Please check email and password', 'danger')
 	return render_template('login.html',
